8ddc4bcc5ac57c92de973efcf6f779714ca30d81
Wei Hsu <wei@airtime.com>
2012-12-08 15:48:10 -0800

    refactor, +less

--- src/leiningen/new/starter.clj
+++ src/leiningen/new/starter.clj
@@ -9,13 +9,14 @@
   (let [data {:name name
               :sanitized (name-to-path name)}]
     (->files data
-             ["project.clj" (render "project.clj" data)];check
-             [".gitignore" (render "gitignore" data)];check
-             ["README.md" (render "README.md" data)];check
-             ["src/{{sanitized}}/server.clj" (render "server.clj" data)];cehck
-             ["src/{{sanitized}}/views/routes.clj" (render "routes.clj" data)];check
-             ["src/{{sanitized}}/views/common.clj" (render "common.clj" data)];check
+             ["project.clj" (render "project.clj" data)]
+             [".gitignore" (render "gitignore" data)]
+             ["README.md" (render "README.md" data)]
+             ["src/{{sanitized}}/server.clj" (render "server.clj" data)]
+             ["src/{{sanitized}}/views/routes.clj" (render "routes.clj" data)]
+             ["src/{{sanitized}}/views/common.clj" (render "common.clj" data)]
              ["src/cljs/main.cljs" (render "main.cljs" data)]
+             ["resources/public/less" (render "styles.less" data)]
              "resources/public/js"
              "resources/public/img"
              "src/{{sanitized}}/models"
